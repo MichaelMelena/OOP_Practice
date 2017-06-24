0907aab88484d25b198dc0a450aea30282e03501
MichaelMelenaPC <michael.melena@gmail.com>
2017-06-24 14:41:17 +0200

    Original version

---
+++ Luky_Cviceni/PointsEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luky_Cviceni
+{
+    class PointsEventArgs:EventArgs
+    {
+        public double MaximumPoints { get; set; }
+        public double CurrentPoints { get; set; }
+    }
+}
